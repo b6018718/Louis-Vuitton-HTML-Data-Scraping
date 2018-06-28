5cb5a3e3913c66d65f9763ea1ed8469c235d6e33
Anthony Dranfield <34162989+b6018718@users.noreply.github.com>
2018-06-28 13:36:22 +0100

    Created Form
    Form created for the application

---
+++ Louis Vuitton/Louis Vuitton/LouisVuittonDataScraping.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Louis_Vuitton
+{
+    public partial class LouisForm : Form
+    {
+        public LouisForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
