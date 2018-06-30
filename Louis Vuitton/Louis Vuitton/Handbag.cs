2fdca9ee6e562576a91df37eace2a84e2bcc6920
Anthony Dranfield <34162989+b6018718@users.noreply.github.com>
2018-06-30 19:19:51 +0100

    Most scraping
    Almost

---
+++ Louis Vuitton/Louis Vuitton/Handbag.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Louis_Vuitton
+{
+    class Handbag
+    {
+        private String productName;
+        private String productCode;
+        private String productImage;
+        private String productDescription;
+        private String productPrice;
+        private String availability;
+        private String pageLink;
+
+        public string ProductName { get => productName; set => productName = value; }
+        public string ProductCode { get => productCode; set => productCode = value; }
+        public string ProductImage { get => productImage; set => productImage = value; }
+        public string ProductDescription { get => productDescription; set => productDescription = value; }
+        public string ProductPrice { get => productPrice; set => productPrice = value; }
+        public string Availability { get => availability; set => availability = value; }
+        public string PageLink { get => pageLink; set => pageLink = value; }
+    }
+}
--- Louis Vuitton/Louis Vuitton/LouisVuittonDataScraping.cs
+++ Louis Vuitton/Louis Vuitton/LouisVuittonDataScraping.cs
@@ -26,11 +26,42 @@ namespace Louis_Vuitton
 
             HtmlAgilityPack.HtmlNodeCollection productNames = webPage.DocumentNode.SelectNodes("//div[@class='productName toMinimize']");
             HtmlAgilityPack.HtmlNodeCollection productPrices = webPage.DocumentNode.SelectNodes("//div[@class='from-price']//span");
-            HtmlAgilityPack.HtmlNodeCollection productCodes = webPage.DocumentNode.SelectNodes("//div[@id='products - grid']");
-            //*[@id="sku_M51419"]/div[2]/div[2]/span
-            //*[@id="sku_M51419"]/div[2]/div[2]
-            //*[@id="sku_M43715"]/div[2]/div[2]/span
+            HtmlAgilityPack.HtmlNodeCollection productPageLinks = webPage.DocumentNode.SelectNodes("//a[@class='product-item tagClick tagClick']");
+            HtmlAgilityPack.HtmlNodeCollection productImageLinks = webPage.DocumentNode.SelectNodes("//a[@class='product-item tagClick tagClick']//div[@class='imageWrapper']//img");
 
+            List<Handbag> handbags = new List<Handbag>();
+            for (int i = 0; i < productNames.Count; i++)
+            {
+                handbags.Add(new Handbag());
+
+                //Product Name
+                handbags.ElementAt(i).ProductName = productNames.ElementAt(i).InnerText;
+
+                //Product Price
+                handbags.ElementAt(i).ProductPrice = productPrices.ElementAt(i).Attributes["data-htmlContent"].Value;
+
+                //Product Code
+                handbags.ElementAt(i).ProductCode = productPageLinks.ElementAt(i).Attributes["data-sku"].Value;
+
+                //Image Link
+                handbags.ElementAt(i).ProductImage = productImageLinks.ElementAt(i).Attributes["data-src"].Value;
+                handbags.ElementAt(i).ProductImage = handbags.ElementAt(i).ProductImage.Replace("IMG_WIDTH", "360");
+                handbags.ElementAt(i).ProductImage = handbags.ElementAt(i).ProductImage.Replace("IMG_HEIGHT", "360");
+
+                //Description
+                handbags.ElementAt(i).PageLink = "https://uk.louisvuitton.com/" + productPageLinks.ElementAt(i).Attributes["href"].Value;
+                webPage = htmlWeb.Load(handbags.ElementAt(i).PageLink);
+                HtmlAgilityPack.HtmlNode description = webPage.DocumentNode.SelectSingleNode("//*[@id='productDescriptionSeeMore']");
+                handbags.ElementAt(i).ProductDescription = description.InnerText;
+
+                //Availability
+                webPage = htmlWeb.Load("https://uk.louisvuitton.com/eng-gb/products/pochette-metis-monogram-empreinte-nvprod630173v#M44072");
+                HtmlAgilityPack.HtmlNode availability = webPage.DocumentNode.SelectSingleNode("//div[@class='notInStockMessage']//span");
+                if (availability.Attributes["data-htmlContent"] != null)
+                    handbags.ElementAt(i).Availability = availability.Attributes["data-htmlContent"].Value;
+                else
+                    handbags.ElementAt(i).Availability = "In Stock";
+            }
         }
     }
 }
