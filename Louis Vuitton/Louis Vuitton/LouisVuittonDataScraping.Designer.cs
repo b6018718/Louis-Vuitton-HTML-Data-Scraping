49b56ec5b564874fc632bdcfb347881d3c9eedbc
Anthony Dranfield <34162989+b6018718@users.noreply.github.com>
2018-07-05 01:28:21 +0100

    UI Improvements
    Getting closer...

--- Louis Vuitton/Louis Vuitton/LouisVuittonDataScraping.Designer.cs
+++ Louis Vuitton/Louis Vuitton/LouisVuittonDataScraping.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.load = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
+            // 
+            // load
+            // 
+            this.load.Location = new System.Drawing.Point(1, 1);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(93, 23);
+            this.load.TabIndex = 0;
+            this.load.Text = "Mine Data";
+            this.load.UseVisualStyleBackColor = true;
+            this.load.Click += new System.EventHandler(this.load_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(117, 1);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(321, 23);
+            this.progressBar.TabIndex = 1;
             // 
             // LouisForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1084, 611);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.load);
             this.Name = "LouisForm";
             this.Text = "Louis Vuitton Data Scraping";
             this.Load += new System.EventHandler(this.LouisForm_Load);
@@ -43,6 +64,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button load;
+        private System.Windows.Forms.ProgressBar progressBar;
     }
 }
 
--- Louis Vuitton/Louis Vuitton/LouisVuittonDataScraping.cs
+++ Louis Vuitton/Louis Vuitton/LouisVuittonDataScraping.cs
@@ -26,29 +26,87 @@ namespace Louis_Vuitton
         
         private void LouisForm_Load(object sender, EventArgs e)
         {
+            progressBar.Minimum = 0;
+            progressBar.Maximum = 100;
+        }
+
+        private GroupBox LoadProduct(Handbag handbag, int xCoord, int yCoord, int boxSize, int handbagNo)
+        {
+            GroupBox groupBox = new GroupBox();
+
+            //Product Image
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Load(handbag.ProductImage);
+            pictureBox.Size = new Size(200, 200);
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            groupBox.Location = new Point(xCoord, yCoord);
+            groupBox.Size = new Size(boxSize, boxSize);
+            groupBox.Name = "handbagNo-" + handbagNo;
+            //this.Controls.Add(pictureBox);
+            groupBox.Controls.Add(pictureBox);
+
+            //Product Name
+            var image = pictureBox.Image;
+            var font = new Font("Times New Roman", 30, FontStyle.Regular, GraphicsUnit.Pixel);
+            var graphics = Graphics.FromImage(image);
+            graphics.DrawString(handbag.ProductName, font, Brushes.Black, new Point(10, 10));
+            pictureBox.Image = image;
+            return groupBox;
+        }
+
+        private void JavaScriptLoaded(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            webSiteLoaded = true;
+        }
+
+        private void load_Click(object sender, EventArgs e)
+        {
+            //Create a box with 4 default spots + more depending on how many bags are present
+            int defaultWidth = 550;
+            int defaultHeight = 550;
+            Panel leftPanel = new Panel
+            {
+                Location = new Point(30, 30),
+                Size = new Size(defaultWidth, defaultHeight),
+                AutoScroll = false,
+                BackColor = Color.Gray,
+            };
+            leftPanel.VerticalScroll.Visible = false;
+
+            this.Controls.Add(leftPanel);
+
+            //Variables for item boxes
+            int boxSize = 200;
+            int xMargin = 40;
+            int yMargin = 50;
+            int columnDistance = 290;
+            int xCoord = 0;
+            int yCoord = 0;
+
+            //Load an instance of Internet Explorer 11 for Web Browsing
             webBrowser.ScriptErrorsSuppressed = true;
-            //Load Browser
             if (!WBEmulator.IsBrowserEmulationSet())
                 WBEmulator.SetBrowserEmulationVersion();
             //Using the HTML Agility NuGet
             HtmlAgilityPack.HtmlWeb htmlWeb = new HtmlWeb();
             HtmlAgilityPack.HtmlDocument webPage = new HtmlAgilityPack.HtmlDocument();
             HtmlAgilityPack.HtmlDocument webPageDesc = new HtmlAgilityPack.HtmlDocument();
-            webBrowser.Navigate("https://uk.louisvuitton.com/eng-gb/women/handbags/_/N-2keomb.html");
+            webPage = htmlWeb.Load("https://uk.louisvuitton.com/eng-gb/women/handbags/_/N-2keomb.html");
 
-            //Description
+            //Web Browser Event
             webBrowser.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(JavaScriptLoaded);
-            while (!webSiteLoaded)
-                Application.DoEvents();
-            var dom = (mshtml.IHTMLDocument3)webBrowser.Document.DomDocument;
-            string html = dom.documentElement.innerHTML;
-            webPage.LoadHtml(html);
 
+            //Collect HTML Data
             HtmlAgilityPack.HtmlNodeCollection productNames = webPage.DocumentNode.SelectNodes("//div[@class='productName toMinimize']");
             HtmlAgilityPack.HtmlNodeCollection productPrices = webPage.DocumentNode.SelectNodes("//div[@class='from-price']//span");
             HtmlAgilityPack.HtmlNodeCollection productPageLinks = webPage.DocumentNode.SelectNodes("//a[@class='product-item tagClick tagClick']");
             HtmlAgilityPack.HtmlNodeCollection productImageLinks = webPage.DocumentNode.SelectNodes("//a[@class='product-item tagClick tagClick']//div[@class='imageWrapper']//img");
 
+            //Progress Bar
+            progressBar.Step = 100 / (productNames.Count + 1);
+            progressBar.PerformStep();
+
+            //Collect JavaScript Data + Add Item to Form
             List<Handbag> handbags = new List<Handbag>();
             for (int i = 0; i < productNames.Count; i++)
             {
@@ -79,14 +137,12 @@ namespace Louis_Vuitton
                 //Out of Stock (For Testing Purposes)
                 //webBrowser.Navigate("https://uk.louisvuitton.com/eng-gb/products/pochette-metis-monogram-empreinte-nvprod630173v#M44072");
 
-                
                 //Description
-                webBrowser.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(JavaScriptLoaded);
                 while (!webSiteLoaded)
                     Application.DoEvents();
                 Uri url = webBrowser.Url;
-                dom = (mshtml.IHTMLDocument3)webBrowser.Document.DomDocument;
-                html = dom.documentElement.innerHTML;
+                var dom = (mshtml.IHTMLDocument3)webBrowser.Document.DomDocument;
+                string html = dom.documentElement.innerHTML;
                 webPageDesc.LoadHtml(html);
 
                 HtmlAgilityPack.HtmlNode description = webPageDesc.DocumentNode.SelectSingleNode("//div[@class='productDescription description-push-text onlyML ppTextDescription ']");
@@ -103,90 +159,28 @@ namespace Louis_Vuitton
                         handbags.ElementAt(i).Availability = "In Stock";
                 }
 
+                //Add Item to Form
+               
+                int rowNumber = Convert.ToInt32(Math.Floor(Convert.ToDouble(i) / 2));
+
+                //Get X & Y Coordinates
+                yCoord = yMargin + rowNumber * boxSize + yMargin * rowNumber;
+                if (i % 2 == 0)
+                    xCoord = xMargin;
+                else
+                    xCoord = columnDistance;
+
+                //Add Box To Panel
+                leftPanel.Controls.Add(LoadProduct(handbags.ElementAt(i), xCoord, yCoord, boxSize, i));
+                progressBar.PerformStep();
             }
-
             //DATA COLLECTED!
+            progressBar.PerformStep();
             webBrowser.Dispose();
-
-            int defaultWidth = 550;
-            int defaultHeight = 550;
-            
-            //Create a box with 4 default spots + more depending on how many bags are present. 300px per 2 bags
-            //int scrollBoxMaxHeight = Convert.ToInt32(defaultHeight + Math.Ceiling((Convert.ToDouble(handbags.Count()) - 4) / 2) * defaultHeight / 2);
-            Panel leftPanel = new Panel
-            {
-                Location = new Point(30, 30),
-                Size = new Size(defaultWidth, defaultHeight),
-                AutoScroll = true,
-                BackColor = Color.Gray,
-                
-            };
+            leftPanel.Height = yCoord + boxSize + yMargin;
+            leftPanel.AutoScroll = true;
             leftPanel.VerticalScroll.Visible = true;
-
-
-            int boxSize = 200;
-            int xMargin = 40;
-            int yMargin = 5;
-            int columnDistance = 290;
-            int handbagNo = 0;
-            foreach (Handbag handbag in handbags)
-            {
-                int xCoord;
-                int yCoord;
-
-                switch (handbagNo)
-                {
-                    case 0:
-                        xCoord = xMargin;
-                        yCoord = yMargin;
-                        break;
-                    case 1:
-                        xCoord = columnDistance;
-                        yCoord = yMargin;
-                        break;
-                    default:
-                        yCoord = Convert.ToInt32(Math.Floor(Convert.ToDouble(handbagNo) / 2)) * boxSize + yMargin;
-                        if (handbagNo % 2 == 0)
-                            xCoord = xMargin;
-                        else
-                            xCoord = columnDistance;
-                        break;
-                }   // End Switch
-                leftPanel.Controls.Add(LoadProduct(handbag, xCoord, yCoord, boxSize, handbagNo));
-                handbagNo++;
-            }   //End For Each Loop
-
-
-            this.Controls.Add(leftPanel);
-        }
-
-        private GroupBox LoadProduct(Handbag handbag, int xCoord, int yCoord, int boxSize, int handbagNo)
-        {
-            GroupBox groupBox = new GroupBox();
-
-            //Product Image
-            PictureBox pictureBox = new PictureBox();
-            pictureBox.Load(handbag.ProductImage);
-            pictureBox.Size = new Size(200, 200);
-            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
-            groupBox.Location = new Point(xCoord, yCoord);
-            groupBox.Size = new Size(boxSize, boxSize);
-            groupBox.Name = "handbagNo-" + handbagNo;
-            //this.Controls.Add(pictureBox);
-            groupBox.Controls.Add(pictureBox);
-
-            //Product Name
-            var image = pictureBox.Image;
-            var font = new Font("Times New Roman", 30, FontStyle.Regular, GraphicsUnit.Pixel);
-            var graphics = Graphics.FromImage(image);
-            graphics.DrawString(handbag.ProductName, font, Brushes.Black, new Point(10, 10));
-            pictureBox.Image = image;
-            return groupBox;
-        }
-
-        private void JavaScriptLoaded(object sender, WebBrowserDocumentCompletedEventArgs e)
-        {
-            webSiteLoaded = true;
+            webSiteLoaded = false;
         }
     }
 }
