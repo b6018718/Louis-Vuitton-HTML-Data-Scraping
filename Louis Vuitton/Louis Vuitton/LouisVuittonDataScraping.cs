bfd68387f19d7f439c8a8728cf8e10e7a000e78b
Anthony Dranfield <34162989+b6018718@users.noreply.github.com>
2018-07-06 05:21:58 +0100

    Penultimate Commit
    Nice work bro

--- Louis Vuitton/Louis Vuitton/LouisVuittonDataScraping.cs
+++ Louis Vuitton/Louis Vuitton/LouisVuittonDataScraping.cs
@@ -23,6 +23,7 @@ namespace Louis_Vuitton
         bool webSiteLoaded = false; //Website Fully Loaded in Memory
         bool loadedOnce = false;    //Button pressed once before during lifetime of application
         bool productsLoaded = false;//When the user is allowed to click on the product
+        bool midwork = false;       //When program is in the middle of mining data
 
         //Variables for the product boxes
         int boxSize = 200;          
@@ -44,62 +45,23 @@ namespace Louis_Vuitton
             progressBar.Maximum = 100;
         }
 
-        private GroupBox LoadProduct(Handbag handbag, int xCoord, int yCoord, int boxSize, int handbagNo)
-        {
-            //Product Image
-            GroupBox groupBox = new GroupBox();
-            PictureBox pictureBox = new PictureBox();
-            pictureBox.Load(handbag.ProductImage);
-            pictureBox.Size = new Size(200, 200);
-            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
-            groupBox.Location = new Point(xCoord, yCoord);
-            groupBox.Size = new Size(boxSize, boxSize);
-            groupBox.Name = "handbagNo-" + handbagNo;
-            groupBox.Controls.Add(pictureBox);
-            groupBox.Click += clickEvent;
-
-            //Product Name
-            var image = pictureBox.Image;
-            var font = new Font("Microsoft Sans Serif", 30, FontStyle.Regular, GraphicsUnit.Pixel);
-            var graphics = Graphics.FromImage(image);
-            graphics.DrawString(handbag.ProductName, font, Brushes.Black, new Point(10, 10));
-            pictureBox.Image = image;
-            pictureBox.Click += clickEvent;
-            return groupBox;
-        }
-
-        private void JavaScriptLoaded(object sender, WebBrowserDocumentCompletedEventArgs e)
-        {
-            webSiteLoaded = true;
-        }
-
         private void load_Click(object sender, EventArgs e)
         {
-            //Create new thread for background work
-            if (loadedOnce)
+            if (!midwork)
             {
-                EraseDisplay();
-                if (this.Controls.ContainsKey("leftPanel"))
-                    this.Controls.Remove(this.Controls["leftPanel"]);
+                midwork = true;
+                //Create new thread for background work
+                if (loadedOnce)
+                {
+                    EraseDisplay();
+                    if (this.Controls.ContainsKey("leftPanel"))
+                        this.Controls.Remove(this.Controls["leftPanel"]);
+                }
+                loadedOnce = true;
+                Thread thread = new Thread(new ThreadStart(WorkerMine_DoWork));
+                thread.SetApartmentState(ApartmentState.STA);
+                thread.Start();
             }
-            loadedOnce = true;
-            Thread thread = new Thread(new ThreadStart(WorkerMine_DoWork));
-            thread.SetApartmentState(ApartmentState.STA);
-            thread.Start();
-        }
-
-        private void LoadWebPageWithBrowser(HtmlAgilityPack.HtmlDocument webPageDesc, String url, WebBrowser webBrowser)
-        {
-            //Navigate Internet Explorer 11 Browser
-            webSiteLoaded = false;
-            webBrowser.Navigate(url);
-
-            //Load DOM Object
-            while (!webSiteLoaded)
-                Application.DoEvents();
-            var dom = (mshtml.IHTMLDocument3)webBrowser.Document.DomDocument;
-            string html = dom.documentElement.innerHTML;
-            webPageDesc.LoadHtml(html);
         }
 
         //MAIN CODE TO LOAD HANDBAGS
@@ -242,7 +204,51 @@ namespace Louis_Vuitton
             this.Invoke(endProgressBar);
             webBrowser.Dispose();
             webSiteLoaded = false;
+            midwork = false;
             productsLoaded = true;
+        }
+
+        private GroupBox LoadProduct(Handbag handbag, int xCoord, int yCoord, int boxSize, int handbagNo)
+        {
+            //Product Image
+            GroupBox groupBox = new GroupBox();
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Load(handbag.ProductImage);
+            pictureBox.Size = new Size(200, 200);
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            groupBox.Location = new Point(xCoord, yCoord);
+            groupBox.Size = new Size(boxSize, boxSize);
+            groupBox.Name = "handbagNo-" + handbagNo;
+            groupBox.Controls.Add(pictureBox);
+            groupBox.Click += clickEvent;
+
+            //Product Name
+            var image = pictureBox.Image;
+            var font = new Font("Microsoft Sans Serif", 30, FontStyle.Regular, GraphicsUnit.Pixel);
+            var graphics = Graphics.FromImage(image);
+            graphics.DrawString(handbag.ProductName, font, Brushes.Black, new Point(10, 10));
+            pictureBox.Image = image;
+            pictureBox.Click += clickEvent;
+            return groupBox;
+        }
+
+        private void JavaScriptLoaded(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            webSiteLoaded = true;
+        }
+
+        private void LoadWebPageWithBrowser(HtmlAgilityPack.HtmlDocument webPageDesc, String url, WebBrowser webBrowser)
+        {
+            //Navigate Internet Explorer 11 Browser
+            webSiteLoaded = false;
+            webBrowser.Navigate(url);
+
+            //Load DOM Object
+            while (!webSiteLoaded)
+                Application.DoEvents();
+            var dom = (mshtml.IHTMLDocument3)webBrowser.Document.DomDocument;
+            string html = dom.documentElement.innerHTML;
+            webPageDesc.LoadHtml(html);
         }
 
         private void InitialseProgressBar(int numberOfSteps)
@@ -292,8 +298,13 @@ namespace Louis_Vuitton
         {
             if (productsLoaded)
             {
-                Panel clickPanel = (Panel) this.Controls["leftPanel"];
+                //Get Left Panel
+                Panel clickPanel = (Panel)this.Controls["leftPanel"];
+
+                //Get Mouse Position
                 Point point = clickPanel.PointToClient(Cursor.Position);
+
+                //Calculate Mouse Position w/ scroll bar
                 int abX = clickPanel.AutoScrollPosition.X * -1;
                 int abY = clickPanel.AutoScrollPosition.Y * -1;
                 point.Offset(new Point(abX, abY));
@@ -301,7 +312,11 @@ namespace Louis_Vuitton
                 int yTopCoord = 0;
                 int xBottonCoord = 0;
                 int yBottomCoord = 0;
-                for (int i = 0; i < handbags.Count; i++)
+
+                //Loop through objects
+                bool found = false;
+                int i = 0;
+                while (i < handbags.Count && !found)
                 {
                     int rowNumber = Convert.ToInt32(Math.Floor(Convert.ToDouble(i) / 2));
                     yTopCoord = yMargin + rowNumber * boxSize + yMargin * rowNumber;
@@ -316,86 +331,94 @@ namespace Louis_Vuitton
                         xTopCoord = columnDistance;
                         xBottonCoord = columnDistance + boxSize;
                     }
-
-                    //Check for collision
+                    //Check for collision for boxes & mouse
                     if (point.X >= xTopCoord && point.X <= xBottonCoord && point.Y >= yTopCoord && point.Y <= yBottomCoord)
                     {
                         //Get rid of current labels & picture boxes
                         EraseDisplay();
 
-                        //Image
-                        PictureBox pictureBox = new PictureBox();
-                        pictureBox.Load(handbags.ElementAt(i).ProductImage);
-                        pictureBox.Size = new Size(306, 306);
-                        pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
-                        pictureBox.Location = new Point(600, 50);
-                        pictureBox.Name = "bigPicture";
-                        this.Controls.Add(pictureBox);
-
-                        //Font
-                        Font sans = new Font("Microsoft Sans Serif", 10, FontStyle.Regular);
-
-                        //Name
-                        Label name = new Label
-                        {
-                            Name = "bigName",
-                            Text = handbags.ElementAt(i).ProductName,
-                            Location = new Point(600, 370),
-                            Font = sans,
-                            AutoSize = true,
-                        };
-                        this.Controls.Add(name);
-
-                        //Product Code
-                        Label code = new Label
-                        {
-                            Name = "bigCode",
-                            Text = handbags.ElementAt(i).ProductCode,
-                            Location = new Point(600, 390),
-                            Font = sans,
-                            AutoSize = true,
-                        };
-                        this.Controls.Add(code);
-
-                        //Price
-                        Label price = new Label()
-                        {
-                            Name = "bigPrice",
-                            Text = handbags.ElementAt(i).ProductPrice,
-                            Location = new Point(600, 410),
-                            Font = sans,
-                            AutoSize = true,
-                        };
-                        this.Controls.Add(price);
-
-                        //Availability
-                        if (handbags.ElementAt(i).Availability != null)
-                        {
-                            Label availability = new Label()
-                            {
-                                Name = "bigAvailability",
-                                Text = handbags.ElementAt(i).Availability,
-                                Location = new Point(600, 430),
-                                Font = sans,
-                                AutoSize = true,
-                            };
-                            this.Controls.Add(availability);
-                        }
-
-                        //Product Description
-                        Label description = new Label()
-                        {
-                            Name = "bigDescription",
-                            Text = handbags.ElementAt(i).ProductDescription,
-                            Location = new Point(600, 450),
-                            AutoSize = true,
-                            MaximumSize = new Size(360,0),
-                            Font = sans,
-                        };
-                        this.Controls.Add(description);
+                        //Set new display on right hand side
+                        SetDisplay(i);
+                        found = true;
                     }
+                    else
+                        i++;
                 }
             }
+        }
+
+        private void SetDisplay(int i)
+        {
+            //Image
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Load(handbags.ElementAt(i).ProductImage);
+            pictureBox.Size = new Size(306, 306);
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox.Location = new Point(600, 50);
+            pictureBox.Name = "bigPicture";
+            this.Controls.Add(pictureBox);
+
+            //Font
+            Font sans = new Font("Microsoft Sans Serif", 10, FontStyle.Regular);
+
+            //Name
+            Label name = new Label
+            {
+                Name = "bigName",
+                Text = handbags.ElementAt(i).ProductName,
+                Location = new Point(600, 370),
+                Font = sans,
+                AutoSize = true,
+            };
+            this.Controls.Add(name);
+
+            //Product Code
+            Label code = new Label
+            {
+                Name = "bigCode",
+                Text = handbags.ElementAt(i).ProductCode,
+                Location = new Point(600, 390),
+                Font = sans,
+                AutoSize = true,
+            };
+            this.Controls.Add(code);
+
+            //Price
+            Label price = new Label()
+            {
+                Name = "bigPrice",
+                Text = handbags.ElementAt(i).ProductPrice,
+                Location = new Point(600, 410),
+                Font = sans,
+                AutoSize = true,
+            };
+            this.Controls.Add(price);
+
+            //Availability
+            if (handbags.ElementAt(i).Availability != null)
+            {
+                Label availability = new Label()
+                {
+                    Name = "bigAvailability",
+                    Text = handbags.ElementAt(i).Availability,
+                    Location = new Point(600, 430),
+                    Font = sans,
+                    AutoSize = true,
+                };
+                this.Controls.Add(availability);
+            }
+
+            //Product Description
+            Label description = new Label()
+            {
+                Name = "bigDescription",
+                Text = handbags.ElementAt(i).ProductDescription,
+                Location = new Point(600, 450),
+                AutoSize = true,
+                MaximumSize = new Size(360, 0),
+                Font = sans,
+            };
+            this.Controls.Add(description);
         }
 
         private void EraseDisplay()
